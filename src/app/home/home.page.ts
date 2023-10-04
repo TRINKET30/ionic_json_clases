1e4380894fde4f8fe40bd89fe5d50214920601b6
duoc <git.duoc@duoc.cl>
2023-10-04 18:15:29 -0300

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  personajes: any = []
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.getPersonajes().subscribe(res => {
+      console.log("respuesta = ", res)
+      this.personajes = res
+    })
+  }
+
+  getPersonajes() {
+    return this.http
+    .get('assets/files/starwars.json')
+    .pipe(
+      map((resp: any) => {
+        return resp.data
+      }) 
+    )
+  }
 
 }
